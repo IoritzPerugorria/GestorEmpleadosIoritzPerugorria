d54f637b148f287705b10c30032c7144dc0b26ca
Ioritz <iperugoald1@educacion.navarra.es>
2024-04-24 18:26:13 +0200

    commit que se me olvido hacer

--- src/main/java/org/example/gestorempleadosioritzperugorria/HelloController.java
+++ src/main/java/org/example/gestorempleadosioritzperugorria/HelloController.java
@@ -1,11 +1,23 @@
 package org.example.gestorempleadosioritzperugorria;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    ObservableList<String> profesiones = FXCollections.observableArrayList(
+        "Scada Manager", "Sales Manager", "Product Owner", "Product Manager", "Analyst Programmer", "Junior Programmer"
+    );
+
+    @FXML
+    private ComboBox<String> combobox = new ComboBox<>(profesiones);
+
 
     @FXML
     protected void onHelloButtonClick() {
